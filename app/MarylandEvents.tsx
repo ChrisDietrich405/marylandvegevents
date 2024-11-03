c6c39ffde950fef388c1b898b1e98063fbe7b748
Chris <chrisdietrich3666@gmail.com>
2024-11-03 07:14:33 -0500

    added navigation

--- app/MarylandEvents.tsx
+++ app/MarylandEvents.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from "react-native";
+import { Button, Text, Card, ButtonGroup } from '@rneui/themed';
 
 export default function MarylandEvents() {
 
     return (
         <View>
 
-            <h1>hello</h1>
+            <Text>hello</Text>
 
         </View>
     );
